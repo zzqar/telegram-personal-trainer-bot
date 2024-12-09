cd41cba07de60a7ea05c6324a38ade91e0c7c47e
lozhkov.v <viktor.lozhkov@netprint.ru>
2024-12-09 23:07:41 +0300

    init

--- internal/cache/redis/redis.go
+++ internal/cache/redis/redis.go
@@ -1,1 +1,35 @@
 package redis
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"telegram-personal-trainer-bot/internal/config"
+)
+
+type StateManager struct {
+	client *redis.Client
+	ctx    context.Context
+}
+
+func MustRedis(conf config.Redis) *StateManager {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     conf.Addr,
+		Password: conf.Password,
+		DB:       conf.DB,
+	})
+
+	ctx := context.Background()
+	return &StateManager{client: rdb, ctx: ctx}
+}
+
+// Set
+// Установить состояние пользователя
+func (r *StateManager) Set(key, state string) error {
+	return r.client.Set(r.ctx, key, state, 0).Err()
+}
+
+// Get
+// Получить состояние пользователя
+func (r *StateManager) Get(key string) (string, error) {
+	return r.client.Get(r.ctx, key).Result()
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+)
+
+type Config struct {
+	Env   string `env:"ENV" env-default:"prod"`
+	Token string `env:"TELEGRAM_TOKEN" env-required:"true"`
+	Redis Redis  `env-prefix:"REDIS_" env-required:"true"`
+}
+
+type Redis struct {
+	Addr     string `env:"ADDR" env-default:"localhost:6379"`
+	Password string `env:"PASSWORD" env-default:""`
+	DB       int    `env:"DB" env-default:"0"`
+}
+
+func MustConfig() *Config {
+	var cfg Config
+	if err := cleanenv.ReadConfig(".env", &cfg); err != nil {
+		log.Fatalf("Error reading config: %v", err)
+	}
+	return &cfg
+}
